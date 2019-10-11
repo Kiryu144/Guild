09978c7c672b49b6498b620d20dee319a768744c
123davidLP@gmail.com <Anton123bertagithub>
2019-10-11 19:35:51 +0200

    Initial commit

---
+++ src/de/mevidia/kiryu144/guild/Guild.java
@@ -0,0 +1,11 @@
+package de.mevidia.kiryu144.guild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Guild extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
